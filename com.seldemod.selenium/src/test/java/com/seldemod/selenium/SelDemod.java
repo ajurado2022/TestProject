1774d4ec099e6b50d375e24a8e8bd029ba39db1f
IOS <IOS@IOS-PC.lan>
2021-11-18 19:33:42 +0800

    My first commit

---
+++ com.seldemod.selenium/src/test/java/com/seldemod/selenium/SelDemod.java
@@ -0,0 +1,11 @@
+package com.seldemod.selenium;
+
+import org.testng.annotations.Test;
+
+public class SelDemod {
+
+	@Test
+	public void printScreen() {
+		System.out.println("My First jenkins project!");
+	}
+}
